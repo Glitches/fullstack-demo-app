ee2f272bca164faf2773780bb4bd85d858756771
Andrea Ceccarelli <gltch2003@gmail.com>
2022-03-28 08:03:34 +0200

    update configs

--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 include .env
 
-.PHONY: setup start sh migrate setup-prod start-prod
+.PHONY: dev-start dev-be migrate prod-setup prod-start
 
-setup:
-		docker-compose build
-start:
-		docker-compose up -d
-sh:
-		docker exec -u node -ti fullstack-node sh
+dev-start:
+		docker-compose --env-file .env up -d
+
+dev-be:
+		docker exec -u node -ti fullstack-be yarn dev
+
 migrate:
 		docker exec -i fullstack-db mysql --user=root --password="$(MYSQL_ROOT_PASSWORD)" --database="$(MYSQL_DATABASE)" < sql/demo_table_create.sql
 		docker exec -i fullstack-db mysql --user=root --password="$(MYSQL_ROOT_PASSWORD)" --database="$(MYSQL_DATABASE)" < sql/fixtures.sql
 
-setup-prod:
+prod-setup:
 		docker-compose -f docker-compose-prod.yml build
 
-start-prod:
+prod-start:
 		docker-compose -f docker-compose-prod.yml up
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,22 @@ declare module 'fastify' {
     }
 }
 
+interface PostUserBody {
+    name: string
+    last_name: string
+}
+
+interface GetUserParams {
+    id: number
+}
+
 const server = fastify()
+
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE
+const MYSQL_USER = process.env.MYSQL_USER
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD
+const MYSQL_HOST = process.env.MYSQL_HOST
+const MYSQL_PORT = process.env.MYSQL_PORT
 
 server.register(fastifyCors, {
     allowedHeaders: '*',
@@ -18,12 +33,8 @@ server.register(fastifyCors, {
 server.register(fastifyMySQL, {
     promise: true,
     type: 'connection',
-    connectionString: 'mysql://user:example-user-password@db-mysql:3306/example-db',
+    connectionString: `mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}`,
 })
-
-interface params {
-    id: number
-}
 
 server.get('/health', async (req, reply) => {
     reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send({ hello: 'world' })
@@ -31,26 +42,22 @@ server.get('/health', async (req, reply) => {
 
 server.get('/user/:id', async (req, _reply) => {
     const connection = await server.mysql
-    server.log.info((req.params as params).id)
+    server.log.info((req.params as GetUserParams).id)
     const [rows] = await connection.query('SELECT id, name, last_name FROM demo WHERE id=?', [
-        (req.params as params).id,
+        (req.params as GetUserParams).id,
     ])
     return rows
 })
 
-interface PostBody {
-    name: string
-    last_name: string
-}
-
 server.post('/user', async (req, reply) => {
-    const body = req.body as PostBody
+    const body = req.body as PostUserBody
     if (body.name === undefined) {
         throw new Error('bad body')
     }
     if (body.name === undefined) {
         throw new Error('bad body')
     }
+
     const connection = await server.mysql
     const result = await connection.query('INSERT INTO demo (name,last_name) VALUES (?, ?)', [
         body.name,
@@ -59,10 +66,14 @@ server.post('/user', async (req, reply) => {
 
     reply.status(200).header('Content-Type', 'application/json; charset=utf-8').send(result)
 })
+
 server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
     if (err) {
-        server.log.error(err)
-        server.close()
+        // eslint-disable-next-line no-console
+        console.error('There was an an error on listening: ', err)
+        // eslint-disable-next-line no-process-exit
+        process.exit()
     }
-    server.log.info(`Server listening at ${address}`)
+
+    console.log(`Server listening at ${address}`)
 })
