28c21b128511bfbaf043fc58ba8ac6dc017468c2
Andrea Ceccarelli <gltch2003@gmail.com>
2022-04-16 16:05:46 +0200

    add io-ts frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,51 @@
+import * as J from "fp-ts/Json";
+import * as TE from "fp-ts/TaskEither";
+import { flow, pipe } from "fp-ts/function";
+import * as t from "io-ts";
 import * as React from "react";
 import "./App.css";
 
+const userResponseCodec = t.type({
+  data: t.array(
+    t.type({
+      id: t.number,
+      name: t.string,
+      last_name: t.string,
+    }),
+    "userResponseCodec"
+  ),
+});
+
+type UserResponse = t.TypeOf<typeof userResponseCodec>;
+
 export const App: React.FC = () => {
-  const [users, setUsers] = React.useState<
-    Array<{ name: string; last_name: string }>
-  >([]);
+  const [users, setUsers] = React.useState<UserResponse["data"]>([]);
+  const [post, setPostResource] = React.useState<Response>();
 
   React.useEffect(() => {
-    fetch(
-      `${
-        import.meta.env.PROD
-          ? import.meta.env.VITE_BE_PROD_URL
-          : import.meta.env.VITE_BE_DEV_URL
-      }/api/user/1`
-    )
-      .then((response) => response.body)
-      .then((rb) => {
-        if (rb === null) {
+    pipe(
+      TE.tryCatch(
+        () =>
+          fetch(
+            `${
+              import.meta.env.PROD
+                ? import.meta.env.VITE_BE_PROD_URL
+                : import.meta.env.VITE_BE_DEV_URL
+            }/api/user`
+          ),
+        () => {
           throw new Error("Error in fetch");
         }
-        const reader = rb.getReader();
+      ),
+      TE.chain((r) => {
+        const rb = r.body;
+        if (rb === null) {
+          return TE.left(new Error("Error in fetch"));
+        }
 
+        return TE.right(rb.getReader());
+      }),
+      TE.map((reader) => {
         return new ReadableStream({
           start(controller) {
             function push() {
@@ -36,16 +61,24 @@ export const App: React.FC = () => {
             push();
           },
         });
+      }),
+      TE.chain((stream) =>
+        TE.tryCatch(
+          () =>
+            new Response(stream, {
+              headers: { "Content-Type": "text/html" },
+            }).text(),
+          (_e) => new Error("Error in extracting text from user stream")
+        )
+      ),
+      TE.chain((r) => pipe(r, J.parse, TE.fromEither)),
+      TE.chainW(flow(userResponseCodec.decode, TE.fromEither)),
+      TE.chainFirst((data) => {
+        setUsers(data.data);
+        return TE.right(data);
       })
-      .then((stream) => {
-        return new Response(stream, {
-          headers: { "Content-Type": "text/html" },
-        }).text();
-      })
-      .then((result) => {
-        setUsers(JSON.parse(result));
-      });
-  }, []);
+    )();
+  }, [post]);
 
   const submitHandler: React.FormEventHandler<HTMLFormElement> = (
     e: React.FormEvent<HTMLFormElement>
@@ -68,49 +101,50 @@ export const App: React.FC = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
       }
-    );
+    ).then((r) => {
+      setPostResource(r);
+    });
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Name list</p>
+        <p>LISTA NOMI</p>
       </header>
-      <div>
+      <div className="table-wrapper">
         <table className="table">
           <thead>
             <tr>
               <td>
-                <b>First name:</b>
+                <b>NOME</b>
               </td>
               <td>
-                <b>Last name:</b>
+                <b>COGNOME</b>
               </td>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              {users.map((u, i) => (
-                <React.Fragment key={`name-${i}`}>
-                  <td key={`name-${i}`}>{u.name}</td>
-                  <td key={`last-name-${i}`}>{u.last_name}</td>
-                </React.Fragment>
-              ))}
-            </tr>
+            {users.map((u, i) => (
+              <tr key={`name-${i}`}>
+                <td key={`name-${i}`}>{u.name}</td>
+                <td key={`last-name-${i}`}>{u.last_name}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
-      <div>
-        <form onSubmit={submitHandler}>
-          <span>nome: </span> <input form="data" name="name" />
-          <span>cognome: </span> <input form="data" name="last_name" />
-          <div>
-            <button type="submit">Crea</button>
+      <div className="form-wrapper">
+        <form className="form" onSubmit={submitHandler}>
+          <div className="form-row">
+            <span>Nome: </span> <input form="data" name="name" />
           </div>
+          <div className="form-row">
+            <span>Cognome: </span> <input form="data" name="last_name" />
+          </div>
+          <button type="submit">Crea</button>
         </form>
       </div>
     </div>
