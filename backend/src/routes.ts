8a384903562a44ed7ec6735f776d6ce90297e94c
Andrea Ceccarelli <gltch2003@gmail.com>
2022-04-16 16:05:21 +0200

    add io-ts backend

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,49 +1,108 @@
 import { FastifyInstance, FastifyPluginOptions, RawReplyDefaultExpression, RawRequestDefaultExpression } from 'fastify'
+import * as TE from 'fp-ts/TaskEither'
+import { identity, pipe } from 'fp-ts/function'
+import * as t from 'io-ts'
 import { Server } from 'http'
 
-interface PostUserBody {
-    name: string
-    last_name: string
-}
+const postUserBodyCodec = t.type({
+    name: t.string,
+    last_name: t.string,
+})
 
-interface GetUserParams {
-    id: number
-}
+const getUserParamsCodec = t.type({
+    id: t.number,
+})
 
 export async function routes(
     server: FastifyInstance<Server, RawRequestDefaultExpression<Server>, RawReplyDefaultExpression<Server>>,
     _options: FastifyPluginOptions
-) {
+): Promise<void> {
     server.get('/health', async (_req, reply) => {
         server.log.info('Healty')
-        reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send({ hello: 'world' })
+        reply.code(200).header('Content-Type', 'application/json; charset=utf-8').send({ healt: 'Healty' })
     })
 
     server.get('/user/:id', async (req, _reply) => {
         const connection = server.mysql
-        server.log.info((req.params as GetUserParams).id)
-        const [rows] = await connection.query('SELECT id, name, last_name FROM demo WHERE id=?', [
-            (req.params as GetUserParams).id,
-        ])
+        const id = getUserParamsCodec.decode(req.params)
+
+        server.log.info(`Requested Id: ${id}`)
+
+        const [rows] = await connection.query('SELECT id, name, last_name FROM demo WHERE id=?', [id])
+
+        server.log.info(rows, 'rows')
         return rows
     })
 
-    server.post('/user', async (req, reply) => {
-        const body = req.body as PostUserBody
-        server.log.info(body)
-        if (body.name === undefined) {
-            throw new Error('bad body')
-        }
-        if (body.name === undefined) {
-            throw new Error('bad body')
-        }
-
+    server.get('/user', async (req, reply) => {
         const connection = server.mysql
 
-        await connection.query('INSERT INTO demo (name,last_name) VALUES (?, ?)', [body.name, body.last_name])
+        return await pipe(
+            TE.tryCatch(
+                async () => {
+                    return await connection.query('SELECT id, name, last_name FROM demo')
+                },
+                _e => {
+                    return {
+                        error: 'there was an error',
+                    }
+                }
+            ),
+            TE.map(rows => rows[0]),
+            TE.fold(
+                (e: { error: string }) => () =>
+                    Promise.resolve(
+                        reply.status(500).header('Content-Type', 'application/json; charset=utf-8').send(e)
+                    ),
+                data => () =>
+                    Promise.resolve(
+                        reply.status(200).header('Content-Type', 'application/json; charset=utf-8').send({
+                            data,
+                        })
+                    )
+            )
+        )()
+    })
 
-        reply.status(200).header('Content-Type', 'application/json; charset=utf-8').send({
-            data: body,
-        })
+    server.post('/user', async (req, reply): Promise<never> => {
+        server.log.info(req.body)
+
+        const connection = server.mysql
+        return await pipe(
+            req.body,
+            postUserBodyCodec.decode,
+            TE.fromEither,
+            TE.chain(b => {
+                return TE.tryCatch(
+                    async () =>
+                        await connection.query('INSERT INTO demo (name,last_name) VALUES (?, ?)', [
+                            b.name,
+                            b.last_name,
+                        ]),
+                    identity
+                )
+            }),
+            TE.fold(
+                (e: unknown) => {
+                    server.log.info(e, 'error')
+                    return () =>
+                        Promise.resolve(
+                            reply
+                                .status(500)
+                                .header('Content-Type', 'application/json; charset=utf-8')
+                                .send({ error: e })
+                        )
+                },
+                b => {
+                    server.log.info(b, 'query')
+                    return () =>
+                        Promise.resolve(
+                            reply.status(200).header('Content-Type', 'application/json; charset=utf-8').send({
+                                data: b,
+                            })
+                        )
+                }
+            )
+        )()
     })
 }
